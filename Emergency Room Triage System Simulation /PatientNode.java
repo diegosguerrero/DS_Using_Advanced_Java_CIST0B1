c7750a6ee510c0402a434a40e9dba7bcd4d6767c
Diego Guerrero <dasg1905@gmail.com>
2026-03-23 22:26:52 -0700

    PatientNode.java

--- Emergency Room Triage System Simulation /PatientNode.java
+++ Emergency Room Triage System Simulation /PatientNode.java
@@ -1,1 +1,19 @@
+public class PatientNode {
 
+    String name;
+    int priorityScore;
+    Long timeArrived;
+
+    // Pointers
+    public PatientNode next;
+    public PatientNode prev;
+
+    public PatientNode(String name, int priorityScore) {
+        this.priorityScore = priorityScore;
+        this.name = name;
+        this.timeArrived = System.currentTimeMillis();
+        this.next = null;
+        this.prev = null;
+
+    }
+}
