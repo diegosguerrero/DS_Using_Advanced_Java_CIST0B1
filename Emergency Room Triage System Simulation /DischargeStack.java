7671177cbb44e70b88826cfc1ade91e19f9859a2
Diego Guerrero <dasg1905@gmail.com>
2026-03-23 22:27:17 -0700

    DischargeStack.java

--- Emergency Room Triage System Simulation /DischargeStack.java
+++ Emergency Room Triage System Simulation /DischargeStack.java
@@ -1,1 +1,27 @@
+public class DischargeStack {
 
+    public Stack<PatientNode> stack = new Stack<>(10);
+
+    public DischargeStack() {
+    }
+
+    public void push(PatientNode node) {
+        stack.push(node);
+    }
+
+    public PatientNode pop() {
+
+        if (stack.isEmtpy()) {
+            return null;
+        }
+        return stack.pop();
+    }
+
+    public PatientNode peek() {
+        if (stack.isEmtpy()) {
+            return null;
+
+        }
+        return stack.peek();
+    }
+}
